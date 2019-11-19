7ce6d866fb8e778eafaf5eac48dde9e5a5059ecc
nichoth <nichoth@gmail.com>
2019-11-19 15:25:20 -0800

    add files

---
+++ src/EVENTS.js
@@ -0,0 +1,7 @@
+var namespace = require('@nichoth/events/namespace')
+
+var EVENTS = namespace({
+    hello: ['world']
+})
+
+module.exports = EVENTS
---
+++ test/index..js
@@ -0,0 +1,6 @@
+var test = require('tape')
+
+test('example', function (t) {
+    t.pass('ok')
+    t.end()
+})
